6495ca09246270f6e1d9b153f9e202d4957895b0
Shreyans Jain <jainshreyans121@gmail.com>
2022-01-17 21:40:03 +0530

    PhoneeypadProblem.cpp

--- 17.PhoneKeypadProblem.cpp
+++ 17.PhoneKeypadProblem.cpp
@@ -1,19 +1,45 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 // TRUST your Function -- Take a LEAP OF FAITH
 // Proof by Mathematical Induction
-void helper() {
+void helper(string str, vector<char> keypad[], string op = "", int i = 0) {
   // Base Case
   // where normal recursive rule is not being followed
+  if(i >= str.size()) {
+    cout<<op<<" ";
+    return;
+  }
 
   // Recursive Call
+  int num = str[i] - '0';
+  int len = keypad[num].size();
+
+  for(int j = 0; j<len; j++) {
+    helper(str, keypad, op + keypad[num][j], i+1);
+  }
 }
 
 int main(int argc, char const *argv[]) {
+  string str;
+  cin>>str;
+
+  vector<char> keypad[] =
+  {
+    {}, {},        // 0 and 1 digit don't have any characters associated
+    { 'a', 'b', 'c' },
+    { 'd', 'e', 'f' },
+    { 'g', 'h', 'i' },
+    { 'j', 'k', 'l' },
+    { 'm', 'n', 'o' },
+    { 'p', 'q', 'r', 's'},
+    { 't', 'u', 'v' },
+    { 'w', 'x', 'y', 'z'}
+  };
 
   // Function Call
-  helper();
+  helper(str, keypad);
 
   return 0;
 }
